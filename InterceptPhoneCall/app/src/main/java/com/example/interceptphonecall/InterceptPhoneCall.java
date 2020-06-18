79ed61d1e9c1abac75c5f977150f50e2b0a31834
isiemaszko <izabela.siemaszko@onet.pl>
2020-06-18 10:55:31 +0200

    first commmit

---
+++ InterceptPhoneCall/app/src/main/java/com/example/interceptphonecall/InterceptPhoneCall.java
@@ -0,0 +1,49 @@
+package com.example.interceptphonecall;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class InterceptPhoneCall extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        StateListener phoneStateListener = new StateListener();
+        TelephonyManager telephonymanager = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        telephonymanager.listen(phoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+    }
+
+    class StateListener extends PhoneStateListener{
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            switch(state){
+                case TelephonyManager.CALL_STATE_RINGING:
+                  //  Toast.makeText( "call "+TelephonyManager.CALL_STATE_RINGING, Toast.LENGTH_SHORT).show();
+
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    break;
+                case TelephonyManager.CALL_STATE_IDLE:
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+
+    }
+}
